fcd3fb0dcc129a17f2f97e37b8181713c429d67b
fpremuz <fpremuz@gmail.com>
2026-03-11 01:10:42 -0300

    added telemetry router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.satellites.api.routes import router as satellite_router
+from app.telemetry.api.routes import router as telemetry_router
 
 app = FastAPI(title="OrbitWatch")
 
 app.include_router(satellite_router)
+app.include_router(telemetry_router)
